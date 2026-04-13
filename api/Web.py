9f9ad4af9c71aea727b2d65cbd9ce6378a887117
Ritik M <ritikmohanbabu@gmail.com>
2026-04-13 15:17:47 +0530

    Update Web.py

--- api/Web.py
+++ api/Web.py
@@ -10,13 +10,13 @@ import os
 import json
 load_dotenv(override=True)
 llm_url = ChatOpenAI(
-model="gemini-2.5-flash-lite",  
+model="gemini-2.5-flash",  
     api_key=os.getenv("GEMINI_API_KEY"),  
     base_url=os.getenv("GEMINI_BASE_URL"),
 )
 
 llm_web = ChatOpenAI(
-model="gemini-2.5-flash-lite",  
+model="gemini-2.5-flash",  
     api_key=os.getenv("GEMINI_API_KEY"),  
     base_url=os.getenv("GEMINI_BASE_URL"),
 
