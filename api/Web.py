afb53e959d953f867f01cc4c22c719fdf1e5c854
Ritik M <ritikmohanbabu@gmail.com>
2026-04-13 12:43:51 +0530

    Change model names in Web.py to 'tinyllama'

--- api/Web.py
+++ api/Web.py
@@ -10,13 +10,13 @@ import os
 import json
 load_dotenv(override=True)
 llm_url = ChatOpenAI(
-model="gemini-2.5-flash",  
+model="gemini-2.5-flas",  
     openai_api_key=os.getenv("GEMINI_API_KEY"),  
     openai_api_base=os.getenv("GEMINI_BASE_URL"),
 )
 
 llm_web = ChatOpenAI(
-model="gemini-2.5-flash",  
+model="tinyllama",  
     openai_api_key=os.getenv("GEMINI_API_KEY"),  
     openai_api_base=os.getenv("GEMINI_BASE_URL"),
 
